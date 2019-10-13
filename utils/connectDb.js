274e9bad1b977f2f133899b32ab1b12804031457
AlexTicovschi <alexmticovschi@gmail.com>
2019-10-13 21:23:18 +0100

    connecting to Mongo Atlas database

---
+++ utils/connectDb.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const connection = {}
+
+const connectDb = async () => {
+    if (connection.isConnected) {
+        // Use existing database connection
+        console.log("Using existing connection");
+        return;
+    }
+
+    // Use new database connection
+    const db = await mongoose.connect(process.env.MONGO_SRV, {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log("DB Connected!");
+    connection.isConnected = db.connections[0].readyState;
+}
+
+export default connectDb;
