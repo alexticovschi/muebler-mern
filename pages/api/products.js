76a9ddb479ff55abc9bd8b74f1726c2377776146
AlexTicovschi <alexmticovschi@gmail.com>
2019-10-14 11:44:39 +0100

    cleaning code from product.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import Product from "../../models/Product";
 import connectDb from "../../utils/connectDb";
-=======
-import products from "../../static/products.json"
-import connectDb from "../../utils/connectDb";
-
-connectDb();
->>>>>>> a4e79d47a2e02a298dc416bcfbad62f7165ff0ad
 
 connectDb();
 
