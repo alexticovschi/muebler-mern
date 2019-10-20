712ba6667587e55d22f5b0a2b06afa6b1fbf7df7
AlexTicovschi <alexmticovschi@gmail.com>
2019-10-20 18:02:06 +0100

    displaying cart items

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,23 +1,57 @@
-import { Header, Segment, Button, Icon } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import { Header, Segment, Item, Button, Icon } from "semantic-ui-react";
 
-const CartItemList = () => {
-  const user = true;
+const CartItemList = ({ products, user }) => {
+  const router = useRouter();
 
-  return (
-    <Segment textAlign="center">
-      <Header icon as="h3">
-        <Icon inverted color="grey" name="shopping basket" />
-        Nothing in your basket yet.
-      </Header>
-      <div>
-        {user ? (
-          <Button color="orange">View Products</Button>
-        ) : (
-          <Button color="blue">Login to Add Products</Button>
-        )}
-      </div>
-    </Segment>
-  );
+  const mapCartProductsToItems = products => {
+    return products.map(p => ({
+      childKey: p.product._id,
+      header: (
+        <Item.Header
+          as="a"
+          onClick={() => router.push(`/product?_id=${p.product._id}`)}
+        >
+          {p.product.name}
+        </Item.Header>
+      ),
+      image: p.product.mediaUrl,
+      meta: `${p.quantity} x ￡${p.product.price}`,
+      fluid: "true",
+      extra: (
+        <Button
+          basic="true"
+          icon="remove"
+          floated="right"
+          onClick={() => console.log(p.product._id)}
+        />
+      )
+    }));
+  };
+
+  if (products.length === 0) {
+    return (
+      <Segment textAlign="center">
+        <Header icon as="h3">
+          <Icon inverted color="grey" name="shopping basket" />
+          Nothing in your basket yet.
+        </Header>
+        <div>
+          {user ? (
+            <Button color="orange" onClick={() => router.push("/")}>
+              View Products
+            </Button>
+          ) : (
+            <Button color="blue" onClick={() => router.push("/login")}>
+              Login to Add Products
+            </Button>
+          )}
+        </div>
+      </Segment>
+    );
+  }
+
+  return <Item.Group divided="true" items={mapCartProductsToItems(products)} />;
 };
 
 export default CartItemList;
